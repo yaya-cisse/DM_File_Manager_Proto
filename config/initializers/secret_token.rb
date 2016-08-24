dd353b9a1f2ed7c25d13f45931894d2f93412605
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-08-24 15:51:55 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DMFileManagerProto::Application.config.secret_token = 'a8499aced87713a6c046b034138f84056fab9a4a1e508864e3aef9d2a70fe70b6ffb4d4396ba88e4cf59e0f805f8a98eb86b997a65a47259ebfd317e778e367a'
