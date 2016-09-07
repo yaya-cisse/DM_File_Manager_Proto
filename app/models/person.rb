bcda3d65f2aa22ec331c8b50b8539b1a76fc9f71
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-09-07 16:50:30 +0200

    Removing two attributes of model

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,8 +3,6 @@ require 'couchbase/model'
 class Person < Couchbase::Model
   attribute :first_name
   attribute :last_name
-  attribute :email
-  attribute :notes
   attribute :image
   attribute :file
 end
