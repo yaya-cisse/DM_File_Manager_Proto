2c241fd307059531fe8b37fce20d4c646376d93f
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-09-16 15:41:08 +0200

    Adding getters and setters for files

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,20 @@
 class Person < ActiveRecord::Base
    attr_accessible :first_name, :last_name, :image, :file
+   after_save :image, :file
+
+  def image
+    Image.find_by(parent: self.first_name)
+  end
+
+  def image=(binary)
+    Image.create(parent: self.first_name, content: Moped::BSON::Binary.new(:generic,binary))
+  end
+
+  def file
+    Fichier.find_by(parent: self.first_name)
+  end
+
+  def file=(binary)
+    Fichier.create(parent: self.first_name, content: Moped::BSON::Binary.new(:generic,binary))
+  end
 end
