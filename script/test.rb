100d184de624f6db0c1267f4c618a218c3be4acc
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-09-16 15:35:51 +0200

    Changing the creation test

--- script/test.rb
+++ script/test.rb
@@ -1,5 +1,5 @@
 def insert
-  Person.create( :first_name => "first_name" , :last_name => "last_name", :file => File.binread("#{Rails.root}/public/file.pdf"))
+  Person.create( :first_name => "first_name1" , :last_name => "last_name", :image => File.binread("#{Rails.root}/public/image.png"), :file => File.binread("#{Rails.root}/public/slack.png"))
 end
 
 puts base = Benchmark.measure { insert }
