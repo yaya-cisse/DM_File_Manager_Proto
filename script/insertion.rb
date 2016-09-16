057cb7e7228ed9859e9569e7bb9add40f8ca0675
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-09-16 10:16:21 +0200

    Changing the create method

--- script/insertion.rb
+++ script/insertion.rb
@@ -1,19 +1,19 @@
 
 def insert_with_two_files
   400.times do |i|
-    Person.create( :first_name => "first_name_#{i}" , :last_name => "last_name_#{i}", :image => Moped::BSON::Binary.new(:generic,File.binread("#{Rails.root}/public/image.png")), :file => Moped::BSON::Binary.new(:generic,File.binread("#{Rails.root}/public/file.pdf"))
+    Person.create( :first_name => "first_name_#{i}" , :last_name => "last_name_#{i}", :image => File.binread("#{Rails.root}/public/image.png"), :file => File.binread("#{Rails.root}/public/file.pdf"))
   end
 end
 
 def insert_with_large_files
   100.times do |i|
-    Person.create( :first_name => "first_name_#{i}" , :last_name => "last_name_#{i}", :file => Moped::BSON::Binary.new(:generic,File.binread("#{Rails.root}/public/file.pdf")))
+    Person.create( :first_name => "first_name_#{i}" , :last_name => "last_name_#{i}", :file => File.binread("#{Rails.root}/public/file.pdf"))
   end
 end
 
 def insert_with_small_files
-  4600.times do |i|
-    Person.create( :first_name => "first_name_#{i}" , :last_name => "last_name_#{i}", :image => Moped::BSON::Binary.new(:generic,File.binread("#{Rails.root}/public/image.png")))
+  4500.times do |i|
+    Person.create( :first_name => "first_name_#{i}" , :last_name => "last_name_#{i}", :image => File.binread("#{Rails.root}/public/image.png"))
   end
 end
 
