4815fc3df44518811807c2ca26b157db06ef776d
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-09-16 16:13:09 +0200

    Adding new test methods

--- script/test.rb
+++ script/test.rb
@@ -2,4 +2,11 @@ def insert
   Person.create( :first_name => "first_name1" , :last_name => "last_name", :image => File.binread("#{Rails.root}/public/image.png"), :file => File.binread("#{Rails.root}/public/slack.png"))
 end
 
-puts base = Benchmark.measure { insert }
+def retrieve_all
+  Person.all
+end
+
+def retrieve_a_file
+  Person.first.image
+end
+puts base = Benchmark.measure { retrieve_a_file }
