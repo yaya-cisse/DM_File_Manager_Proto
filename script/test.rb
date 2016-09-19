f4681db12b7b479a6fbf57e0cdb07431d524f57e
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-09-19 10:34:42 +0200

    writing benchmarking result into the result file

--- script/test.rb
+++ script/test.rb
@@ -2,4 +2,12 @@ def insert
   Person.create( :first_name => "first_name" , :last_name => "last_name", :file => Moped::BSON::Binary.new(:generic, File.binread("#{Rails.root}/public/file.pdf") ))
 end
 
-puts base = Benchmark.measure { insert }
+# puts base = Benchmark.measure { insert }
+
+# File.write("#{Rails.root}/public/result.txt", "userCPU   systemCPU   total    elapsedRealTime")
+File.open("#{Rails.root}/public/result.txt", 'w') do |file|
+  file.write("  userCPU    systemCPU   total    elapsedRealTime")
+  file.write(base = Benchmark.measure { insert })
+  file.write(base = Benchmark.measure { retrieve_all })
+  file.close
+end
