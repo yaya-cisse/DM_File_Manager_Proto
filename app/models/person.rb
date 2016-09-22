16331a916993ce4c19d782b0735e3a126d62b280
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-09-22 18:25:03 +0200

    Removing image getter/setter

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,14 +1,4 @@
 class Person < ActiveRecord::Base
-   attr_accessible :first_name, :last_name, :image, :file
-   before_create :image, :file
-
-  def image
-    Image.find_by(parent: self.first_name)
-  end
-
-  def image=(binary)
-    Image.create(parent: self.first_name, content: Moped::BSON::Binary.new(:generic,binary))
-  end
 
   def file
     Fichier.find_by(parent: self.first_name)
