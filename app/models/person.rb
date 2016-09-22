474e945f63f04584ea45ac9d81c47a6bc219595b
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-09-22 18:25:32 +0200

    Changing file getters and setters

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,10 +1,26 @@
 class Person < ActiveRecord::Base
+   attr_accessible :first_name, :last_name, :file, :file_id, :file_type
+   # before_initialize :init
 
   def file
-    Fichier.find_by(parent: self.first_name)
+    @file ||= Fichier.find(self.file_id)
   end
 
   def file=(binary)
-    Fichier.create(parent: self.first_name, content: Moped::BSON::Binary.new(:generic,binary))
+    unless self.file_id
+      file_content = Fichier.new()
+      self.file_id = file_content.id.to_s
+    else
+      file_content = Fichier.find(self.file_id)
+    end
+    file_content.content = Moped::BSON::Binary.new(:generic,binary)
+    file_content.save
   end
+
+  # private
+
+  # def init
+  #   self.file_id = Fichier.new().id
+  # end
+
 end
