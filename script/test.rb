5ff9a6da30b66ec147b9d7ccd2217c46cbbb5526
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-09-23 11:57:39 +0200

    Adding 10 times and destroy all

--- script/test.rb
+++ script/test.rb
@@ -1,5 +1,7 @@
 def insert
-  Person.create( :first_name => "first_name1" , :last_name => "last_name", :file => File.binread("#{Rails.root}/public/slack.png"))
+  10.times do
+    Person.create( :first_name => "first_name1" , :last_name => "last_name", :file => File.binread("#{Rails.root}/public/slack.png"))
+  end
 end
 
 def retrieve_all
@@ -10,10 +12,16 @@ def retrieve_a_file
   Person.first.image
 end
 
+def destroy_all
+  Person.destroy_all
+  Fichier.destroy_all
+end
+
 # File.write("#{Rails.root}/public/result.txt", "userCPU   systemCPU   total    elapsedRealTime")
 File.open("#{Rails.root}/public/result.txt", 'w') do |file|
   file.write("  userCPU    systemCPU   total    elapsedRealTime")
   file.write(base = Benchmark.measure { insert })
   file.write(base = Benchmark.measure { retrieve_all })
+  file.write(base = Benchmark.measure { destroy_all })
   file.close
 end
