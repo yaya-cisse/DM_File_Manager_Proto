a3d305d71d6a28ee568020ca0cbb2f0b24d9cacf
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-09-23 11:32:59 +0200

    Changing tests

--- script/test.rb
+++ script/test.rb
@@ -1,5 +1,5 @@
 def insert
-  Person.create( :first_name => "first_name1" , :last_name => "last_name", :image => File.binread("#{Rails.root}/public/image.png"), :file => File.binread("#{Rails.root}/public/slack.png"))
+  Person.create( :first_name => "first_name1" , :last_name => "last_name", :file => File.binread("#{Rails.root}/public/slack.png"))
 end
 
 def retrieve_all
