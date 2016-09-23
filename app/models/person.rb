da3151b3568cf259618c66c629b4c881fc8601b1
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-09-23 11:25:36 +0200

    Adding getters and setters

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,18 @@
 class Person < ActiveRecord::Base
-   attr_accessible :first_name, :last_name, :image, :file
+   attr_accessible :first_name, :last_name, :file_type, :file_id, :file
+
+  def file
+    @file ||= Fichier.find(self.file_id) if self.file_id
+  end
+
+  def file=(binary)
+    unless self.file_id
+      file_content = Fichier.create()
+      self.file_id = file_content.id
+    else
+      file_content = Fichier.find(self.file_id)
+    end
+    file_content.content = binary
+    file_content.save
+  end
 end
