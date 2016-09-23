c2375be302a137920ab0022ee997c7f90e630135
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-09-23 16:27:02 +0200

    Adding destroy method

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,6 @@
 class Person < ActiveRecord::Base
-   attr_accessible :first_name, :last_name, :file_type, :file_id, :file
+  attr_accessible :first_name, :last_name, :file_type, :file_id, :file
+  after_destroy :destroy_file
 
   def file
     @file ||= Fichier.find(self.file_id) if self.file_id
@@ -15,4 +16,18 @@ class Person < ActiveRecord::Base
     file_content.content = binary
     file_content.save
   end
+
+  private
+
+  # def init
+  #   self.file_id = Fichier.new().id
+  # end
+  #
+  def destroy_file
+    if self.file_id
+      file_content = Fichier.find(self.file_id)
+      file_content.destroy
+    end
+  end
+
 end
