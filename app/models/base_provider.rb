841919a9957fcdf38259837f66644318b4bf54fb
yaya-cisse <yaya-cisse.abdelhafiz@mobile-devices.fr>
2016-10-05 12:05:10 +0200

    Changing the definition of the methods

--- app/models/base_provider.rb
+++ app/models/base_provider.rb
@@ -1,11 +1,18 @@
 class BaseProvider
 
-  def self.get_file
+  #method to get the file given the file id as a string
+  def self.get_file(id)
   end
 
-  def self.set_file
+  #method to set the file given the file id as a string and the binary content of the file
+  def self.set_file(id, binary)
   end
 
-  def self.set_file
+  #method to save the file
+  def self.save_file
+  end
+
+  #method to destroy the file given the file id as a string
+  def self.destroy_file(id)
   end
 end
